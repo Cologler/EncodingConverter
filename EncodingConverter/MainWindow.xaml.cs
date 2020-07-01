f20528f1c7e46e4f82b72dd1fc0a418a2059ea73
Cologler <skyoflw@gmail.com>
2020-07-02 04:31:53 +0800

    添加项目文件。

---
+++ EncodingConverter/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using UtfUnknown;
+
+namespace EncodingConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            ComboBoxItem selected = null;
+            var encodings = Encoding.GetEncodings().Select(z => z.GetEncoding());
+            foreach (var encoding in encodings)
+            {
+                var encodingItem = new ComboBoxItem
+                {
+                    Content = encoding.EncodingName,
+                    DataContext = encoding
+                };
+                if (encoding == Encoding.UTF8)
+                {
+                    selected = encodingItem;
+                }
+                this.TargetEncoding.Items.Add(encodingItem);
+            }
+            this.TargetEncoding.SelectedItem = selected ?? this.TargetEncoding.Items.OfType<object>().FirstOrDefault();
+
+            this.FilesListView.ItemsSource = this.Items;
+        }
+
+        public Encoding SelectedEncoding
+        {
+            get
+            {
+                return (Encoding)((ComboBoxItem)this.TargetEncoding.SelectedItem).DataContext;
+            }
+        }
+
+        private async void SingleConvertButton_Click(object sender, RoutedEventArgs e)
+        {
+            await ((TextFileViewModel)((FrameworkElement)sender).DataContext).ConvertAsync(this.SelectedEncoding);
+        }
+
+        private async void ListView_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Copy;
+                var paths = (string[])e.Data.GetData(DataFormats.FileDrop);
+                var vms = paths.Where(z => File.Exists(z)).Select(z => new TextFileViewModel(z)).ToList();
+                vms.ForEach(this.Items.Add);
+                await Task.WhenAll(vms.Select(z => z.DetectAsync()).ToArray());
+            }
+        }
+
+        ObservableCollection<TextFileViewModel> Items { get; } = new ObservableCollection<TextFileViewModel>();
+    }
+}
---
+++ EncodingConverter/TextFileViewModel.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using UtfUnknown;
+
+namespace EncodingConverter
+{
+    class TextFileViewModel : INotifyPropertyChanged
+    {
+        private string _encodingName;
+        private bool _isEnabledConvert;
+        private string _convertStatus;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = default)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public TextFileViewModel(string path)
+        {
+            this.Path = path;
+            this.ConvertStatus = "Convert";
+        }
+
+        public async Task DetectAsync()
+        {
+            var dr = await Task.Run(() =>
+            {
+                return CharsetDetector.DetectFromFile(this.Path);
+            });
+
+            var encoding = dr.Detected?.Encoding;
+            if (encoding != null)
+            {
+                this.EncodingName = dr.Detected.EncodingName;
+                this.Encoding = dr.Detected.Encoding;
+                this.IsEnabledConvert = true;
+            }
+        }
+
+        public string Path { get; }
+
+        public string EncodingName
+        {
+            get => this._encodingName;
+            private set
+            {
+                if (this._encodingName != value)
+                {
+                    this._encodingName = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+
+        public Encoding Encoding { get; private set; }
+
+        public string ConvertStatus
+        {
+            get => this._convertStatus;
+            private set
+            {
+                if (this._convertStatus != value)
+                {
+                    this._convertStatus = value;
+                    this.OnPropertyChanged();
+                }                
+            }
+        }
+
+        public bool IsEnabledConvert
+        {
+            get => this._isEnabledConvert;
+            private set
+            {
+                if (this._isEnabledConvert != value)
+                {
+                    this._isEnabledConvert = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+
+        public async Task ConvertAsync(Encoding targetEncoding)
+        {
+            if (targetEncoding is null)
+                throw new ArgumentNullException(nameof(targetEncoding));
+            this.IsEnabledConvert = false;
+            var path = this.Path;
+            await Task.Run(() =>
+            {
+                var newName = System.IO.Path.GetFileNameWithoutExtension(path);
+                newName += $".{targetEncoding.WebName.ToLower()}";
+                newName += System.IO.Path.GetExtension(path);
+                var newPath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(path), newName);
+                using var reader = new StreamReader(this.Path, this.Encoding);
+                using var writer = new StreamWriter(newPath, false, targetEncoding);
+                writer.Write(reader.ReadToEnd());
+            });
+            this.ConvertStatus = "Done";
+        }
+    }
+}
